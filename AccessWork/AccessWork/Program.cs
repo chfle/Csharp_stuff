7c9c6b19e1410f1992737dfd709d96efe309138f
chfle <christian.lehnert.home@gmail.com>
2021-12-06 15:32:58 +0100

    Read XML and send email

--- AccessWork/AccessWork/Program.cs
+++ AccessWork/AccessWork/Program.cs
@@ -6,7 +6,7 @@ namespace AccessWork
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
--- XMLSendEmail/XMLSendEmail/Program.cs
+++ XMLSendEmail/XMLSendEmail/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Net;
+using System.Net.Mail;
+using System.Text.RegularExpressions;
+using System.Xml;
 
 namespace XMLSendEmail
 {
@@ -6,7 +10,71 @@ namespace XMLSendEmail
     {
         static void Main(string[] args)
         {
+            // GET XML DATA
+            string from = "";
+            string to = "";
+            string mail = "";
+            string subject = "";
+            var xmlObj = XmlReader.Create(@"C:\Users\ChristianLehnert\Downloads\chsharp_stuff\XMLSendEmail\XMLSendEmail\mail.xml");
             
+            // read mail
+            while (xmlObj.Read())
+            {
+                if (xmlObj.NodeType == XmlNodeType.Element)
+                {
+                    switch (xmlObj.Name)
+                    {
+                        case "from":
+                            from = xmlObj.ReadString();
+                            break;
+                        case "to":
+                            to = xmlObj.ReadString();
+                            break;
+                        case "message":
+                            mail = xmlObj.ReadString();
+                            break;
+                        case "subject":
+                            subject = xmlObj.ReadString();
+                            break;
+                    }
+                }
+            }
+            
+            // Get Names
+            string fromName = "";
+            string ToName = "";
+
+            var R = new Regex("([^@]+)");
+
+            fromName = R.Match(from).Value;
+            ToName = R.Match(to).Value;
+
+            if (fromName.Length > 0|| ToName.Length > 0)
+            {
+                var fromAddress = new MailAddress(from, fromName);
+                var toAddress = new MailAddress(to, ToName);
+                
+                // get password
+                Console.WriteLine("Enter a Password");
+                string passwd = Console.ReadLine();
+
+                var smtp = new SmtpClient
+                {
+                    Host = "smtp.gmail.com",
+                    Port = 587,
+                    EnableSsl = true,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    UseDefaultCredentials = false,
+                    Credentials = new NetworkCredential(fromAddress.Address, passwd)
+                };
+
+                using var message = new MailMessage(fromAddress, toAddress)
+                {
+                    Subject = subject,
+                    Body = mail
+                };
+                smtp.Send(message);
+            }
         }
     }
 }
