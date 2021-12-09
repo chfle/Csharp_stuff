21e7847b761ccf3c0b6bd5e6743094ef4b3f7873
chfle <christian.lehnert.home@gmail.com>
2021-12-09 13:38:26 +0100

    Person DB with Windows DB

--- AccessWork/AccessWork/Program.cs
+++ AccessWork/AccessWork/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Data.OleDb;
+using System.Linq.Expressions;
 
 namespace AccessWork
 {
@@ -6,7 +8,37 @@ namespace AccessWork
     {
         static void Main(string[] args)
         {
-            
+            OleDbConnection connection = new OleDbConnection();
+            OleDbCommand cmd = new OleDbCommand();
+
+            OleDbDataReader reader;
+
+            // connection
+            connection.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;" +
+                                          @"Data Source=C:\Users\ChristianLehnert\Downloads\chsharp_stuff\AccessWork\AccessWork\Firma21.accdb";
+
+            cmd.Connection = connection;
+            cmd.CommandText = "SELECT * FROM person";
+
+            try
+            {
+                connection.Open();
+                reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Console.WriteLine($"{reader["name"]}, {reader["vorname"]}, {reader["personalnummer"]}, " +
+                                      $"{reader["gehalt"]}, {reader["geburtstag"]}");
+                }
+
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            // C:\Users\ChristianLehnert\Downloads
         }
     }
 }
