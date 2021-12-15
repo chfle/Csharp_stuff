ccd1f655d03a01e4f24da82c496136dabca1303b
chfle <christian.lehnert.home@gmail.com>
2021-12-15 18:42:45 +0100

    validate pass

--- UserLogin/PasswdValid.cs
+++ UserLogin/PasswdValid.cs
@@ -3,14 +3,64 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
 
 namespace UserLogin
 {
     class PasswdValid
     {
+        // create a hash of a string of chars
+       public static int iterator = 10000;
         public static string HashString(string value)
         {
-            
+            byte[] salt;
+
+            // generate salt
+            new RNGCryptoServiceProvider().GetBytes(salt = new byte[16]);
+
+            // hash and salt password
+            var passHash = new Rfc2898DeriveBytes(value, salt, iterator);
+
+            // place string of bytes to store it
+            byte[] hash = passHash.GetBytes(20);
+
+            byte[] hashBytes = new byte[36];
+
+            // place hash and salt
+            Array.Copy(salt, 0, hashBytes, 0, 16);
+            Array.Copy(hash, 0, hashBytes, 16, 20);
+
+            // convert hash to string
+            return Convert.ToBase64String(hashBytes);
+        }
+
+        public static bool isPassword(string passwordText, string passwordHash)
+        {
+            // convert password hash to byte
+            byte[] hashBytes = Convert.FromBase64String(passwordHash);
+
+            // get salt from string
+            byte[] salt = new byte[16];
+            Array.Copy(hashBytes, 0, salt, 0, 16);
+
+            // hash password text
+            var passText = new Rfc2898DeriveBytes(passwordText, salt, iterator);
+
+            // put hash 
+            byte[] hash = passText.GetBytes(20);
+
+            // validate hash
+            bool valid = true;
+
+            // go over each byte and check if valid
+            for (int i = 0; i < 20; i++) {
+                if (hashBytes[i+16] != hash[i])
+                {
+                    valid = false;
+                }
+            }
+
+            return valid;
         }
 
     }
--- UserLogin/Program.cs
+++ UserLogin/Program.cs
@@ -7,6 +7,7 @@ namespace UserLogin
     {
         static void Main(string[] args)
         {
+            // Database connection
             OleDbConnection connection = new OleDbConnection();
             OleDbCommand cmd = new OleDbCommand();
 
@@ -16,8 +17,10 @@ namespace UserLogin
             connection.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;" +
                                           @"Data Source=C:\Users\ChristianLehnert\Downloads\chsharp_stuff\UserLogin\UserLogin\UserLogin.accdb";
 
+            string passHash = PasswdValid.HashString("Chris");
 
-
+            Console.WriteLine(PasswdValid.isPassword("Chris", passHash));
+            Console.WriteLine(PasswdValid.isPassword("Chri$", passHash));
         }
     }
 }
