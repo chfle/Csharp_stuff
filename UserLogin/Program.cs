d118a0c8618420256f115fc5e5d2062c16470197
chfle <christian.lehnert.home@gmail.com>
2021-12-15 18:28:03 +0100

    password hash

---
+++ UserLogin/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data.OleDb;
+
+namespace UserLogin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OleDbConnection connection = new OleDbConnection();
+            OleDbCommand cmd = new OleDbCommand();
+
+            OleDbDataReader reader;
+
+            // connection
+            connection.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;" +
+                                          @"Data Source=C:\Users\ChristianLehnert\Downloads\chsharp_stuff\UserLogin\UserLogin\UserLogin.accdb";
+
+
+
+        }
+    }
+}
